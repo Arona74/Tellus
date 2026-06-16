7ae0b9f6b3280f23df9f3cb5b04a8e30e2e81785
Vojtěch Šokala <vojtasokala@gmail.com>
2026-06-16 17:35:20 +0200

    Lower 1.12.2 render mixin priority for Nothirium support

--- cleanroom/src/main/java/com/seibel/distanthorizons/cleanroom/mixins/client/MixinRenderGlobal.java
+++ cleanroom/src/main/java/com/seibel/distanthorizons/cleanroom/mixins/client/MixinRenderGlobal.java
@@ -33,7 +33,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(RenderGlobal.class)
+@Mixin(value = RenderGlobal.class, priority = 900)
 public class MixinRenderGlobal
 {
 	@Shadow private WorldClient world;
